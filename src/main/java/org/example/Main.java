908017b463338b7ee8775f697fd066216c86f475
Sophia Okosodo <sophieokosodo@gmail.com>
2022-12-22 20:30:10 +0100

    Created Student View

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,107 +4,14 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-        StudentService studentfunc = new StudentService();
-        StudentModel student = new StudentModel();
-        Scanner input = new Scanner(System.in);
 
-        String proceed = "yes";
+        StudentView view = new StudentView();
 
-        System.out.println("|========================================================================|");
-        System.out.println("|======================== JAMB REGISTERATION FORM =======================|");
-        System.out.println("|========================================================================|");
-
-        while (proceed.equalsIgnoreCase("yes")){
-
-            System.out.println();
-            System.out.println("<<1>> Add Students ");
-            System.out.println("<<2>> View All Students ");
-            System.out.println("<<3>> View student By Id");
-            System.out.println("<<4>> Edit student");
-            System.out.println("<<5>> View students by course");
-            System.out.println("<<6>> View Students of required minimum age");
-            System.out.println("<<7>> Exit");
-
-            int response = optionValidation();
-
-            if(response == 1){
-                System.out.println();
-                System.out.println(">> Please enter the Student's Registeration Number");
-                student.setRegNo(input.next());
-                System.out.println(">> Please enter the Student's First Name");
-                student.setFirstName(input.next());
-                System.out.println(">> Please enter the Student's Last Name");
-                student.setLastName(input.next());
-                System.out.println(">> Please enter the Student's age");
-                student.setAge(input.nextInt());
-                System.out.println(">> Please enter the Student's Course");
-                student.setCourse(input.next());
-                System.out.println();
-                System.out.println(studentfunc.addStudent(student.getRegNo(), student));
-            }
-            else if(response == 2){
-                System.out.println();
-                System.out.println(studentfunc.viewAllStudents(student));
-            }
-            else if(response == 3){
-                System.out.println();
-                System.out.println(">>> Please Enter the Student's id");
-                String id = input.next();
-                System.out.println(studentfunc.viewStudentById(id, student));
-            }
-            else if(response == 4){
-                System.out.println();
-                System.out.println(">>> Please Enter the Student's Registration number you want to edit");
-                String id = input.next();
-                System.out.println(">> Please enter the Student's First Name");
-                student.setFirstName(input.next());
-                System.out.println(">> Please enter the Student's Last Name");
-                student.setLastName(input.next());
-                System.out.println(">> Please enter the Student's age");
-                student.setAge(input.nextInt());
-                System.out.println(">> Please enter the Student's Course");
-                student.setCourse(input.next());
-                System.out.println();
-                System.out.println(studentfunc.editStudent(id, student));
-            }
-            else if(response == 5){
-                System.out.println();
-                System.out.println(">>> Please enter the course");
-                String course = input.next();
-                System.out.println(studentfunc.viewStudentByCourse(course, student));
-            }
-            else if(response == 6){
-                System.out.println();
-                System.out.println(">>> Please Enter the minimum Age");
-                int age = input.nextInt();
-                System.out.println(studentfunc.viewStudentOfGivenMinimumAge(age, student));
-            }
-
-
-            if(response == 7){
-                System.out.println("Thank you");
-                proceed = "no";
-
-            }
-            else{
-                System.out.println("\nDo you want to continue?");
-                proceed = input.next();
-            }
-        }
+        view.StudentConsole();
 
     }
 
-    public static int optionValidation(){
-        int response = input.nextInt();
-        while(response < 1 || response > 7){
-            System.out.println("Please Input a valid number");
-            response = input.nextInt();
-        }
 
-        return  response;
-    }
 
 }
